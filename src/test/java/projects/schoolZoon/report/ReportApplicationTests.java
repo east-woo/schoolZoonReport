ae6b34b8d0887030656aed9a75b7b1c0cca291d9
east-woo <dwseo96@naver.com>
2023-08-01 00:29:07 +0900

    first commit

---
+++ src/test/java/projects/schoolZoon/report/ReportApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.schoolZoon.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
